a4fdc7622ad8e32cb6817d449d2ecbe0331520bf
James Hariady Widjaja <widjajajames01@gmail.com>
2023-01-14 17:38:29 -0500

    Updates

---
+++ precache-manifest.28dfeb31029b835a05e72e241b1351b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9fe1b43a263a064af6",
+    "url": "/static/css/main.5a18b689.chunk.css"
+  },
+  {
+    "revision": "8d9fe1b43a263a064af6",
+    "url": "/static/js/main.f04b02a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3eb718c1adc413a3c00c",
+    "url": "/static/js/2.95c2e7c4.chunk.js"
+  },
+  {
+    "revision": "4617f133ac5dd5a741717cfa7517315e",
+    "url": "/index.html"
+  }
+];
